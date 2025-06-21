58c7eddad2530d5d8c5072f756632424efaaa57d
orji Dominion <orjidominion32@gmail.com>
2025-06-21 17:37:11 +0100

    hommy

--- app/page.tsx
+++ app/page.tsx
@@ -24,30 +24,30 @@ const Page = () => {
       <section className="fixed top-0 left-0 bottom-0 w-full h-full flex justify-center px-5 pt-10
                           md:justify-end md:pr-10 md:pt-10 overflow-y-auto">
         <div className="w-full max-w-[30rem] flex flex-col mb-auto mt-8">
-          <div className='p-8 bg-[#f9f9f9]/90'>
+          <div className='p-10 bg-[#f9f9f9]/90'>
             <div>
-              <h1 className='font-bold text-[52px] lg:tracking-[0.8em] tracking-[0.4em]'>BREEZY</h1>
+              <h1 className='font-bold lg:text-[52px] text-[48px] lg:tracking-[0.8em] tracking-[0.4em]'>BREEZY</h1>
             </div>
-            <div>
-              <h2 className='text-[#A1ACAF] font-semibold tracking-[1em] text-lg'>COLLECTION</h2>
+            <div className='flex text-center mx-auto md:text-start'>
+              <h2 className='text-[#A1ACAF] font-semibold tracking-[1em] text-md lg:text-lg'>COLLECTION</h2>
             </div>
             <div className='mt-[1rem]'>
-              <p className='text-[#454545] text-[13px] max-w-sm leading-[1.8rem]'>
+              <p className='text-[#454545] text-[13px] md:max-w-sm leading-[1.4rem] md:leading-[1.8rem]'>
                 <span className='text-[#454545] font-bold'>Lorem Ipsum </span>
                 is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 It has survived not only five centuries, but a
               </p>
-              <p className='text-[#454545] text-[13px] mt-[1rem] max-w-sm leading-[1.8rem]'>
+              <p className='text-[#454545] text-[13px] mt-[1rem] md:max-w-sm leading-[1.4rem] md:leading-[1.8rem]'>
                 ever since the 1500s, when an unknown printer took a galley of type and scrambled it
                 to make a type specimen book. It has survived not only five centuries, but a
               </p>
-              <p className='text-[#454545] text-[13px] mt-[1rem] max-w-sm leading-[1.8rem]'>
+              <p className='text-[#454545] text-[13px] mt-[1rem] md:max-w-sm leading-[1.4rem] md:leading-[1.8rem]'>
                 ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
                 a type specimen book. It has survived not only five centuries, but a
               </p>
-              <p className='text-[#454545] text-[13px] mt-[1rem] max-w-sm leading-[1.8rem]'>
+              <p className='text-[#454545] text-[13px] mt-[1rem] md:max-w-sm leading-[1.4rem] md:leading-[1.8rem]'>
                 ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 It has survived not only five centuries, but a ever since the 1500s, when an unknown 
               </p>
